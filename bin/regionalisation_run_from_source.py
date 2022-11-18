b7e0be201caee1456f8f0e4f738e758b6f98999a
Corentin Carton De Wiart <corentin.carton@ecmwf.int>
2022-11-18 15:35:02 +0000

    debug regionalisation

--- bin/regionalisation_run_from_source.py
+++ bin/regionalisation_run_from_source.py
@@ -1,11 +1,12 @@
+#!/usr/bin/env python3
 import os
 import shutil
+import argparse
 import lisf1
 
 
 if __name__ == '__main__':
 
-    print("=================== START ===================")
     parser = argparse.ArgumentParser()
     parser.add_argument('--source', help='Source catchments')
     parser.add_argument('--target', help='Target catchments')
@@ -28,26 +29,31 @@ if __name__ == '__main__':
         print('***************************************************************************************************')
         print(f"Set up and compute the LISFLOOD {run_type}")
 
+        # Get the settings file from the source
         source_xml = os.path.join(args.source, "settings", f'settings_lisflood{run_type}long_term_run.xml')
-        target_xml = os.path.join(args.target, "settings", f'settings_lisflood{run_type}long_term_run.xml')
         with open(source_xml, "r") as f:
             source_template = f.read()
             if '<textvar name="Gauges" value=' in source_template:  
                 source_start = source_template.index('<textvar name="Gauges" value=')
                 source_end = source_template.index('-7.85 41.15 -7.75 41.15 -7.35 41.15 -7.25 41.15 -7.15 41.15 -7.05 41.15 -5.85 41.75 # Duoro')
-                
+        
+        # Get the settings file from the target
+        # The requires the long term run setting files to be in the target directory
+        target_xml = os.path.join(args.target, "settings", f'settings_lisflood{run_type}long_term_run.xml')
         with open(target_xml, "r") as f:
             target_template = f.read()
             if '<textvar name="Gauges" value=' in target_template:  
                 target_start = target_template.index('<textvar name="Gauges" value=')
                 target_end = target_template.index('-7.85 41.15 -7.75 41.15 -7.35 41.15 -7.25 41.15 -7.15 41.15 -7.05 41.15 -5.85 41.75 # Duoro')         
 
-        target_template = target_template.replace(target_template[target_start:target_end], source_template[source_start:source_end])     
-        target_template = target_template.replace(f'{args.source}', f'{args.target}')      
-        target_template = target_template.replace("out/long_term_run", "out/args.regionalisation")
+        # Replace gauges, PathRoot and PathOut values from target settings in source settings (where all the parameters are defined)
+        region_template = source_template
+        region_template = region_template.replace(source_template[source_start:source_end], target_template[target_start:target_end])
+        region_template = region_template.replace(f'"{args.source}"', f'"{args.target}"')      
+        region_template = region_template.replace("out/long_term_run", f"out/{args.regionalisation}")
 
         regionalisation_xml = os.path.join(args.target, "settings", f'settings_lisflood{run_type}_{args.regionalisation}.xml')
         with open(regionalisation_xml, "w") as f:
-            f.write(target_template)
+            f.write(region_template  )
 
-        # lisf1.main(regionalisation_xml)
+        lisf1.main(regionalisation_xml)
