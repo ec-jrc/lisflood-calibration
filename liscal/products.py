92672bea00299499df2a6ad1255bb1f9272cc7f1
Oisín Morrison <oisinmmm@gmail.com>
2025-03-26 13:01:21 +0000

    add option not to save plots in create_products

--- liscal/products.py
+++ liscal/products.py
@@ -13,7 +13,7 @@ from matplotlib import ticker
 from liscal import hydro_stats, thresholds, evaluation
 
 
-def create_products(cfg, subcatch, obj):
+def create_products(cfg, subcatch, obj, save=True):
     """
     Creates various hydrological evaluation products including statistical summaries, plots, and tables.
 
@@ -41,8 +41,9 @@ def create_products(cfg, subcatch, obj):
     obs_start = datetime.strptime(subcatch.data['Obs_start'],"%d/%m/%Y %H:%M").strftime('%d/%m/%Y %H:%M')
     obs_end = datetime.strptime(subcatch.data['Obs_end'],"%d/%m/%Y %H:%M").strftime('%d/%m/%Y %H:%M')
 
-    # create output directory
-    os.makedirs(cfg.summary_path, exist_ok=True)
+    if save:
+        # create output directory
+        os.makedirs(cfg.summary_path, exist_ok=True)
 
     # Long term run has run_id X
     simulated_streamflow = obj.read_simulated_streamflow_best()
@@ -56,45 +57,54 @@ def create_products(cfg, subcatch, obj):
     # get return periods at station coordinates
     return_periods = thresholds.compute_thresholds(simulated_streamflow)
     # thresholds = xr.open_dataset(cfg.return_periods).sel(x=subcatch.data['LisfloodX'], y=subcatch.data['LisfloodY'])
-    print(return_periods)
+    if save:
+        print(return_periods)
 
-    # create asci output of stats
-    with open(os.path.join(subcatch.path_out, 'stats.txt'), 'w') as f:
-        f.write(str(stats))
-        f.close()
+        # create asci output of stats
+        with open(os.path.join(subcatch.path_out, 'stats.txt'), 'w') as f:
+            f.write(str(stats))
+            f.close()
 
     # create speedometer plots
     speedo = evaluation.SpeedometerPlot(cfg.plot_params)
-    speedo.plot(stats, os.path.join(subcatch.path_out, 'speedo'))
-    os.system('convert {0}.svg {0}.pdf'.format(os.path.join(subcatch.path_out, 'speedo')))
+    speedo_fig = speedo.plot(stats, os.path.join(subcatch.path_out, 'speedo') if save else None)
+    if save:
+        os.system('convert {0}.svg {0}.pdf'.format(os.path.join(subcatch.path_out, 'speedo')))
 
     # create box plot
     box = evaluation.MonthlyBoxPlot(cfg.plot_params)
-    box.plot(sim_monthly, obs_monthly, os.path.join(subcatch.path_out, 'boxy'))
-    os.system('convert {0}.svg {0}.pdf'.format(os.path.join(subcatch.path_out, 'boxy')))
+    box_fig = box.plot(sim_monthly, obs_monthly, os.path.join(subcatch.path_out, 'boxy') if save else None)
+    if save:
+        os.system('convert {0}.svg {0}.pdf'.format(os.path.join(subcatch.path_out, 'boxy')))
 
     # create time series plot
     ts = evaluation.TimeSeriesPlot(cfg.plot_params)
-    ts.plot(Q.index, Q['Sim'].values, Q['Obs'].values, return_periods, os.path.join(subcatch.path_out, 'timmy'))
-    os.system('convert {0}.svg {0}.pdf'.format(os.path.join(subcatch.path_out, 'timmy')))
+    ts_fig = ts.plot(Q.index, Q['Sim'].values, Q['Obs'].values, return_periods, os.path.join(subcatch.path_out, 'timmy') if save else None)
+    if save:
+        os.system('convert {0}.svg {0}.pdf'.format(os.path.join(subcatch.path_out, 'timmy')))
 
     # create Q-Q plot
     qq = evaluation.QQPlot(cfg.plot_params)
-    qq.plot(Q.index, Q['Sim'].values, Q['Obs'].values, os.path.join(subcatch.path_out, 'qqy'))
-    os.system('convert {0}.svg {0}.pdf'.format(os.path.join(subcatch.path_out, 'qqy')))
+    qq_fig = qq.plot(Q.index, Q['Sim'].values, Q['Obs'].values, os.path.join(subcatch.path_out, 'qqy') if save else None)
+    if save:
+        os.system('convert {0}.svg {0}.pdf'.format(os.path.join(subcatch.path_out, 'qqy')))
     
     # create best parameters table plot
     bestparmtrs = evaluation.BestParamPlot(cfg.plot_params)
-    bestparmtrs.plot(os.path.join(subcatch.path, 'pareto_front.csv'), os.path.join(subcatch.path_out, 'bestparmtrs'))
-    os.system('convert {0}.png {0}.pdf'.format(os.path.join(subcatch.path_out, 'bestparmtrs')))
+    bestparmtrs_fig = bestparmtrs.plot(os.path.join(subcatch.path, 'pareto_front.csv'), os.path.join(subcatch.path_out, 'bestparmtrs') if save else None)
+    if save:
+        os.system('convert {0}.png {0}.pdf'.format(os.path.join(subcatch.path_out, 'bestparmtrs')))
 
     # create (inter)catchment plot
     spatialplot = evaluation.SpatialPlot(cfg.plot_params)
-    spatialplot.plot(os.path.join(subcatch.path, 'maps'), os.path.join(subcatch.path_out, 'spatial'))
-    os.system('convert {0}.png {0}.pdf'.format(os.path.join(subcatch.path_out, 'spatial')))
+    spatialplot_fig = spatialplot.plot(os.path.join(subcatch.path, 'maps'), os.path.join(subcatch.path_out, 'spatial') if save else None)
+    if save:
+        os.system('convert {0}.png {0}.pdf'.format(os.path.join(subcatch.path_out, 'spatial')))
     
     # compute contingency table and export
     # contingency_values = binary_scores.contingency_table(thresholds, Q)
     # contingency_df = pd.DataFrame(data=contingency_values, index=subcatch.obsid)
     # print(contingency_df)
     # contingency_df.to_csv(path.join(cfg.summary_path, 'contingency_table_{}.csv'.format(subcatch.obsid)))
+
+    return speedo_fig, box_fig, ts_fig, qq_fig, bestparmtrs_fig, spatialplot_fig
